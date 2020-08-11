cea30a122a6f47477443c5a8377100645821c6a4
Joseph Manangan <joseph.m.manangan@accenture.com>
2020-08-11 08:59:02 +0000

    Changes :  - Remove un-needed panels in navigation  - Add Nav elements for Main Pages  - Change the image of static user  - Added Icons for Anne B Lance ,Hospital and Operator  - Remove Razor syntax for now in _Layout  - Change page title  - Addition of @Viewbag Title in Layout for dynamic page title  - Change site Logo in Navigation  - Added CSS and JS Bundling.

--- iSmartRescue/App_Start/BundleConfig.cs
+++ iSmartRescue/App_Start/BundleConfig.cs
@@ -8,23 +8,22 @@ namespace iSmartRescue
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/vendor/jquery/jquery.min.js",
+                        "~/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                        "~/vendor/jquery-easing/jquery.easing.min.js", 
+                        "~/js/sb-admin-2.min.js",
+                        "~/vendor/chart.js/Chart.min.js", 
+                        "~/js/demo/chart-area-demo.js",
+                        "~/js/demo/chart-pie-demo.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.validate*"));
 
-            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
-            //// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
+               bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/css/sb-admin-2.min.css",
+                      "~/font.css"));
+            bundles.Add(new StyleBundle("~/Content/fontawesome").Include(
+                      "~/vendor/fontawesome-free/css/all.min.css"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js"));
-
-            //bundles.Add(new StyleBundle("~/Content/css").Include(
-            //          "~/Content/bootstrap.css",
-            //          "~/Content/site.css"));
         }
     }
 }
